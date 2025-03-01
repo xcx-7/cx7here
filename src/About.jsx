bb6e02dc0fe15c503936a1b17dca4efd9e44a843
mooon-dev <zainab.42847@gmail.com>
2025-03-01 10:23:32 +0600

    ✈

---
+++ src/About.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className="page">
+      <h1>About Me</h1>
+      <p>I am a developer who loves creating innovative apps and projects. I enjoy turning ideas into digital reality!</p>
+    </div>
+  );
+}
+
+export default About;
